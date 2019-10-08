fcce9f644a0add6f7c8619a4c18ccbc13f3ca6e8
salem84 <salem84@users.noreply.github.com>
2019-10-08 23:45:55 +0200

    migration to azure iothub device client 2.0

--- custom_components/azure_iot_hub/__init__.py
+++ custom_components/azure_iot_hub/__init__.py
@@ -1,8 +1,6 @@
 """
 Support for Azure IoT Hub.
 
-For more details about this platform, please refer to the documentation at
-https://home-assistant.io/components/???
 """
 import logging
 import voluptuous as vol
@@ -20,8 +18,7 @@ ATTR_NAME = 'name'
 DEFAULT_NAME = 'Azure IoT Hub'
 
 SEND_CALLBACKS = 0
-MESSAGE_TEMPLATE = "{{\"device\":\"{}\",\"entity\":\"{}\",\"state\":\"\"}}"
-
+MESSAGE_TEMPLATE = "{{\"device\":\"{}\",\"entity\":\"{}\",\"state\":\"{}\"}}"
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -56,33 +53,52 @@ CONFIG_SCHEMA = vol.Schema({
 }, extra=vol.ALLOW_EXTRA)
 
 
-def setup(hass, config):
+async def async_setup(hass, config):
     """Set up is called when Home Assistant is loading our component."""
-    import iothub_client
-    from iothub_client import IoTHubClient, IoTHubClientError, IoTHubTransportProvider, IoTHubClientResult
-    from iothub_client import IoTHubMessage, IoTHubMessageDispositionResult, IoTHubError, DeviceMethodReturnValue
+    from azure.iot.device.aio import IoTHubDeviceClient
+    from azure.iot.device import Message
+    import asyncio
+    import uuid
 
-    @callback
-    def _send_confirmation_callback(message, result, user_context):
-        _LOGGER.info("Confirmation[%d] received for message with result = %s" % (
-            user_context, result))
+    async def iothub_client_init(config, device):
+        
+        conn = "HostName={};DeviceId={};SharedAccessKey={}".format(config[DOMAIN].get("host"), device[0], device[1].get("auth_key"))
 
-    def iothub_client_init(config, device):
-        conn = "HostName={}.azure-devices.net;DeviceId={};SharedAccessKey={}".format(
-            config[DOMAIN].get("host"), device[0], device[1].get("auth_key"))
+        #_LOGGER.info("Device connection using connection string {}".format(conn))
+        device_client = IoTHubDeviceClient.create_from_connection_string(conn)
 
-        client = IoTHubClient(conn, IoTHubTransportProvider.MQTT)
+        # Connect the client.
+        await device_client.connect()
 
-        # # set the time until a message times out
-        client.set_option("messageTimeout",
-                          config[DOMAIN].get(CONF_MESSAGE_TIMEOUT))
-        client.set_option("logtrace", config[DOMAIN].get(CONF_LOG_LEVEL))
-        return client
+        return device_client
+
+    # import iothub_client
+    # from iothub_client import IoTHubClient, IoTHubClientError, IoTHubTransportProvider, IoTHubClientResult
+    # from iothub_client import IoTHubMessage, IoTHubMessageDispositionResult, IoTHubError, DeviceMethodReturnValue
+
+    # @callback
+    # def _send_confirmation_callback(message, result, user_context):
+    #     _LOGGER.info("Confirmation[%d] received for message with result = %s" % (
+    #         user_context, result))
+
+    # def iothub_client_init(config, device):
+    #     conn = "HostName={}.azure-devices.net;DeviceId={};SharedAccessKey={}".format(
+    #         config[DOMAIN].get("host"), device[0], device[1].get("auth_key"))
+
+    #     client = IoTHubClient(conn, IoTHubTransportProvider.MQTT)
+
+    #     # # set the time until a message times out
+    #     client.set_option("messageTimeout",
+    #                       config[DOMAIN].get(CONF_MESSAGE_TIMEOUT))
+    #     client.set_option("logtrace", config[DOMAIN].get(CONF_LOG_LEVEL))
+    #     return client
 
 
-    def _publish_to_azure(entity_id, old_state, new_state):
+    async def _publish_to_azure(entity_id, old_state, new_state):
+        #_LOGGER.debug("_publish_to_azure with old_state: {}, new_state: {}".format(old_state, new_state))
         if new_state is None:
             return
+
         global CLIENTS
         global MESSAGE_COUNTER
 
@@ -91,44 +107,46 @@ def setup(hass, config):
             if not include or any(entity_id in t for t in include):
                 devicename = device[0]
                 client = CLIENTS.get(devicename)
-                _LOGGER.warn("device {} recieved an update for {}".format(devicename, entity_id))
-                message = IoTHubMessage(MESSAGE_TEMPLATE.format(
-                    config[DOMAIN].get(CONF_DEVICE_ID), entity_id, new_state.state))
+                _LOGGER.debug("device {} received an update for {}".format(devicename, entity_id))
+                body = MESSAGE_TEMPLATE.format(
+                    config[DOMAIN].get(CONF_DEVICE_ID), entity_id, new_state.state)
+                _LOGGER.debug("Message Body {}".format(body))
+                message = Message(body)
 
-                client.send_event_async(
-                    message, _send_confirmation_callback, MESSAGE_COUNTER)
+                await client.send_message(message) #, _send_confirmation_callback, MESSAGE_COUNTER)
 
-                hass.states.set("azureiot.{}".format(slugify(devicename)), 'initialized',
+                hass.states.async_set("azureiot.{}".format(slugify(devicename)), 'initialized',
                                 {'error_message': '',
                                  'count': MESSAGE_COUNTER})
                 MESSAGE_COUNTER += 1
 
-    # Initialize devices
+    # # Initialize devices
 
     for device in config[DOMAIN]["devices"].items():
         try:
             devicename = device[0]
-            CLIENTS[device[0]] = iothub_client_init(config, device)
-            hass.states.set("azureiot.{}".format(slugify(device[0])), 'initialized',
-                            {'error_message': '',
-                             'count': 0})
+
+            _LOGGER.debug("azureiot.{}".format(slugify(device[0])))
+
+            CLIENTS[device[0]] = await iothub_client_init(config, device)
+            hass.states.async_set("azureiot.{}".format(slugify(device[0])), 'initialized', {'error_message': '', 'count': 0})
+            #hass.states.set("azureiot.{}".format(slugify(device[0])), 'initialized', {'error_message': '', 'count': 0})
 
             include = device[1].get(CONF_INCLUDE)
             if not include:
                 _LOGGER.info("Setting up {} to listen for all events".format(devicename))
-                async_track_state_change(
-                    hass, include, _publish_to_azure)
+                async_track_state_change(hass, include, _publish_to_azure)
             else:
                 _LOGGER.info("Setting up {} to listen for {}".format(devicename, include))
-                async_track_state_change(
-                    hass, MATCH_ALL, _publish_to_azure)
+                async_track_state_change(hass, MATCH_ALL, _publish_to_azure)
 
-        except IoTHubClientError as iot_error:
+        except Exception as iot_error: #IoTHubClientError as iot_error:
+            #_LOGGER.error("Azure IoT error:")
             _LOGGER.error("Azure IoT error: {}".format(iot_error))
-            hass.states.set("azureiot.{}".format(slugify(device[0])), 'error', {
+            hass.states.async_set("azureiot.{}".format(slugify(device[0])), 'error', {
                 'error_message': "{}".format(iot_error)
             })
 
     # Return boolean to indicate that initialization was successfully.
+    _LOGGER.info("Azure IoT started")
     return True
-
